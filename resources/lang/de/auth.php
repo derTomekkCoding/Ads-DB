1df9fe7c5e4d26ed96d9c657e9011d1bd7033711
AlpixTM <contact@alpix.eu>
2017-10-31 13:23:54 +0100

    + de

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // DE by Alpix
+
+    'failed' => 'Diese Zugangsdaten wurden nicht in useren Datensätzen gefunden.', //These credentials do not match our records
+    'throttle' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds erneut.',//Too many login attempts. Please try again in :seconds seconds.
+
+];
---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    // DE by Alpix
+
+    'password' => 'Passwörter müssen mindestens 6 Zeichen lang sein und als korrekt bestätigt werden.', //Passwords must be at least six characters and match the confirmation.
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',     //Your password has been reset!
+    'sent' => 'Wir haben dir deinen Link zum Zurücksetzen des Passwortes per E-Mail geschickt!', //We have e-mailed your password reset link!
+    'token' => 'Der Passwort-Wiederherstellungs-Schlüssel ist ungültig.', //This password reset token is invalid.
+    'user' => "Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.", //We can't find a user with that e-mail address.
+
+];
