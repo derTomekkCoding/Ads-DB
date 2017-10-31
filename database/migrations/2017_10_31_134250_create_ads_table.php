9f4bb8269deee9d106e811929c3c928edaa53661
AlpixTM <contact@alpix.eu>
2017-10-31 14:46:02 +0100

    Add db schema

---
+++ database/migrations/2017_10_31_134250_create_ads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('creator_id');
+            $table->integer('advertiser_id');
+            $table->integer('platform_id');
+            $table->integer('type_id');
+            $table->string('url',255);
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
